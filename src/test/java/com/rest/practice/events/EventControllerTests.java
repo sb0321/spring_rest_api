86791f8384e0c368b8147f2f86ae92e20b117204
sb0321 <woltaryksb1@gmail.com>
2021-05-23 17:34:45 +0900

    test: POST /api/events return 201 test type-safe

--- src/test/java/com/rest/practice/events/EventControllerTests.java
+++ src/test/java/com/rest/practice/events/EventControllerTests.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -16,8 +17,7 @@ import java.time.LocalDateTime;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 @ExtendWith(SpringExtension.class)
@@ -38,10 +38,10 @@ public class EventControllerTests {
         Event event = Event.builder()
                 .name("Spring")
                 .description("REST API Development with Spring")
-                .beginEventDateTime(LocalDateTime.of(2019, 11, 12, 22,32))
-                .closeEnrollmentDateTime(LocalDateTime.of(2019, 11, 13, 22,32))
-                .beginEventDateTime(LocalDateTime.of(2019, 11, 13, 22,32))
-                .endEventDateTime(LocalDateTime.of(2019, 11, 14, 22,32))
+                .beginEventDateTime(LocalDateTime.of(2019, 11, 12, 22, 32))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019, 11, 13, 22, 32))
+                .beginEventDateTime(LocalDateTime.of(2019, 11, 13, 22, 32))
+                .endEventDateTime(LocalDateTime.of(2019, 11, 14, 22, 32))
                 .basePrice(100)
                 .maxPrice(200)
                 .limitOfEnrollment(100)
@@ -54,10 +54,11 @@ public class EventControllerTests {
         mockMvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaTypes.HAL_JSON)
-                .content(objectMapper.writeValueAsString(event))
-        )
+                .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
                 .andExpect(status().isCreated())
-                .andExpect(jsonPath("id").exists());
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
     }
 }
